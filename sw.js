d66112544639f259dd923501dacc0ae646264fbf
GauthierCrp <gauthiercrp@outlook.fr>
2026-02-16 23:03:47 +0100

    fix smatphone bug

--- sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('install', (e) => {
-    console.log('Service Worker installé !');
-  });
-  
-  self.addEventListener('fetch', (e) => {
-    // Nécessaire pour valider le critère PWA, 
-    // même si on ne fait rien de spécial ici.
-    e.respondWith(fetch(e.request));
-  });
